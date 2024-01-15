4bfa26a2abfd7d69f4cfa808e3caa6fe5b5ebe37
Gokul <gokulchandan24@gmail.com>
2024-01-15 13:13:37 +0530

    backend side done

---
+++ Backend/index.js
@@ -0,0 +1,41 @@
+import express from "express";
+import mysql2 from "mysql2";
+const app = express();
+
+const db = mysql2.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "qwerty",
+  database: "test",
+});
+
+app.get("/", (req, res) => {
+  res.json("Hello World!");
+});
+
+app.use(express.json());
+app.get("/books", (req, res) => {
+  const q = "SELECT * FROM test.test";
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+
+    res.json(data);
+  });
+});
+
+app.post("/books", (req, res) => {
+  //   const a =
+  //     "INSERT INTO `test`.`test` (`id`, `title`, `desc`, `cover`) VALUES ('2', 'cover title 2', 'desc 2', '2cover.png')";
+  const a =
+    "INSERT INTO `test`.`test` (`id`, `title`, `desc`, `cover`) VALUES (?)";
+  const values = [req.body.id, req.body.title, req.body.desc, req.body.cover];
+  db.query(a, [values], (err, data) => {
+    if (err) return res.json(err);
+
+    res.json("Book has been created successfully");
+  });
+});
+
+app.listen(8800, () => {
+  console.log("server is running");
+});
